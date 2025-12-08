b60d26084a54d1c2e9f5bd95579b83c6f13ccb22
JGWill <jgi@jgwill.com>
2025-12-08 05:18:22 -0500

    rispecs from claude haiku miadisabelle/TandT#14

---
+++ LAUNCH__session_id__RISEFrameworkMiadiTandT_2512080430.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+. _env.sh
+export SESSION_ID=cbc5c7eb-5992-4a17-8fbe-563207c74bb4
+export session_id__RISEFrameworkMiadiTandT_2512080430
+export session_id__RISEFrameworkMiadiTandT_2512080430__MCP_CONFIG
+export session_id__RISEFrameworkMiadiTandT_2512080430__ADD_DIR
+claude "apply the rise framework to create all specs in ./rispecs" --mcp-config $session_id__RISEFrameworkMiadiTandT_2512080430__MCP_CONFIG --add-dir $session_id__RISEFrameworkMiadiTandT_2512080430__ADD_DIR --session-id $session_id__RISEFrameworkMiadiTandT_2512080430  --model haiku \
+	--allow-dangerously-skip-permissions
+
+
---
+++ _env.sh
@@ -0,0 +1,8 @@
+  
+# Namespace: RISEFrameworkMiadiTandT 
+session_id__RISEFrameworkMiadiTandT_2512080430=cbc5c7eb-5992-4a17-8fbe-563207c74bb4
+alias cdRISEFrameworkMiadiTandT="cd /src/_sessiondata/cbc5c7eb-5992-4a17-8fbe-563207c74bb4 && ls -ltra && . session_bash_func_aliases.sh&>/dev/null" 
+alias cdPlansRISEFrameworkMiadiTandT='cd /src/_sessiondata/cbc5c7eb-5992-4a17-8fbe-563207c74bb4/plans 2>/dev/null || echo "No plans directory found."; ls -ltra; echo "Enter plan filename to open:"; read planfile; if [ -f "$planfile" ]; then nano "$planfile"; else echo "File does not exist."; fi' 
+session_id__RISEFrameworkMiadiTandT_2512080430__MCP_CONFIG="/src/.mcp.coaiapy.env.aetherial.json"
+session_id__RISEFrameworkMiadiTandT_2512080430__ADD_DIR="/src/llms/"
+  
